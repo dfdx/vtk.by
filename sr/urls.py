e7b41ca08cb33866a2abf05cee946731e7d31597
Andrei Zhabinski <faithlessfriend@gmail.com>
2016-09-06 01:13:50 +0300

    shynkage ryu now supports free style pages

--- sr/urls.py
+++ sr/urls.py
@@ -6,4 +6,5 @@ app_name = 'sr'
 urlpatterns = [
     url(r'^$', views.info, name='sr-info'),
     url(r'^info/$', views.info, name='sr-info'),
+    url(r'^', views.viewpage, name='viewpage'),
 ]
--- sr/views.py
+++ sr/views.py
@@ -1,9 +1,41 @@
 
-from django.shortcuts import render, redirect
+from django.shortcuts import HttpResponse, render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .models import *
+from vt.models import *
 
 def info(req):
     return render(req, 'sr/info.html', {})
 
+# free form pages
+
+def viewpage(req):
+    slug = req.path.rsplit('/', 1)[-1]
+    pages = Page.objects.filter(slug=slug)
+    if len(pages) == 1:        
+        return render(req, 'sr/page.html', {'page': pages[0]})
+    else:
+        return render(req, 'sr/page-doesnt-exist.html', {'slug': slug})
+
+
+@login_required(login_url='/login')
+def editpage(req):
+    slug = req.GET['slug']
+    pages = Page.objects.filter(slug=slug)
+    if len(pages) == 1:
+        return render(req, 'sr/editpage.html', {'page': pages[0]})
+    else:
+        new_page = Page(slug=slug, text='', lang='markdown')
+        return render(req, 'sr/editpage.html', {'page': new_page})
+        
+
+
+@login_required(login_url='/login')
+def savepage(req):
+    slug = req.POST['slug']
+    Page.objects.update_or_create({
+        'text': req.POST['text'],
+        'lang':  req.POST['lang']
+    }, slug=slug)
+    # hack: both prefixes - /sr and /vt - will show the same page
+    return redirect('/sr/' + req.POST['slug'])
